e05900866ff2d0edd33417d5a2fd01f354c5336d
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-09 01:20:50 +0800

    solve tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fheit) {
+  let celsius = (fheit - 32) * 5/9;
+  return Math.round(celsius * 10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let fheit = celsius * 9/5 + 32;
+  return Math.round(fheit * 10)/10;
 };
 
 // Do not edit below this line
@@ -11,3 +13,5 @@ module.exports = {
   ftoc,
   ctof
 };
+
+console.log(ftoc(40), ctof(40));
