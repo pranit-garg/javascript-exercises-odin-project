35f85cc2dbc4066e1bc5a7535d057f0fc51392a3
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-09 00:23:48 +0800

    solve number 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,24 @@
-const leapYears = function() {
+const leapYears = function(yearToTest) {
+    
+    if (yearToTest % 400 == 0) {
+        return true;
+    } else if (yearToTest % 100 == 0) {
+        return false; 
+    } else if (yearToTest % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+    
+    /* if (yearToTest % 4 == 0) {
+        if (yearToTest % 100 == 0) {
 
+        }
+        return true;
+    } else {
+        return false;
+    } */ 
 };
-
 // Do not edit below this line
 module.exports = leapYears;
