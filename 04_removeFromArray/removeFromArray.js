e5d27b8e5c9dd430cf3469a4f1cb8e69b710804b
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-09 00:05:29 +0800

    solve number 5 (skip 4 for now)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let returnArray = array;
 
+    for (let i = 0; i < returnArray.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (array[i] == args[j]) {
+                continue;
+            }
+        }
+    }
+    return returnArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+console.log(removeFromArray([2, 3, 4, 5, 6], 5, 6, 2))
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let smallerNum;
+    let largerNum;
+
+    if (num1 < 0 || num2 < 0 || typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    }
+
+    if (num1 < num2) {
+        smallerNum = num1;
+        largerNum = num2;
+    } else {
+        smallerNum = num2;
+        largerNum = num1;
+    }
+
+
+
+    for (let i = smallerNum; i <= largerNum; i++) {
+        sum += i;
+    }
+    return sum;
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+console.log(sumAll(1, 4));
