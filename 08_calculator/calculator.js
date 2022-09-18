4ffaa51eac17b030d4ae81346a7cefd2188c940e
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-19 06:19:58 +0800

    solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let numberToAdd = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    numberToAdd += arguments[i];
+  }
+  return numberToAdd;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(sumArr) {
+  let numberToSum = 0;
+  for (let i = 0; i < sumArr.length; i++) {
+    numberToSum += sumArr[i];
+  }
+  return numberToSum;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArr) {
+  let numbersToMultiply = 1;
+  for (let i = 0; i < multiplyArr.length; i++) {
+    numbersToMultiply *= multiplyArr[i];
+  }
+  return numbersToMultiply;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNumber) {
+  let returnedFactorial = 1;
+    while (factorialNumber > 1) {
+      returnedFactorial *= factorialNumber;
+      factorialNumber--;
+    }
+    return returnedFactorial;
 };
 
 // Do not edit below this line
@@ -31,3 +48,5 @@ module.exports = {
   power,
   factorial
 };
+
+console.log(subtract(10, 4));
