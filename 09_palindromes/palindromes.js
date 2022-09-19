29bd969531bc0e916376f45b9ca77d828c99528b
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-19 08:36:08 +0800

    solve palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let letterMatchRegex = /[a-zA-Z]/
+    let arrayToTest = [];
+    for (let i = 0; i < string.length; i++) {
+        if (letterMatchRegex.test(string[i])) {
+            arrayToTest.push(string[i]);
+        }
+    }
+    let palindromeToTest = arrayToTest.join('')
+    let palindromeToTestLower = palindromeToTest.toLowerCase();
 
+    console.log(palindromeToTestLower);
+
+    for (let i = 0, j = palindromeToTestLower.length - 1; i < palindromeToTestLower.length; i++, j--) {
+        let goingForward = palindromeToTestLower[i];
+        let goingBackwards = palindromeToTestLower[j];
+        if (goingForward !== goingBackwards) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+palindromes("RaCecar!")
