f96c9ff8b41d8020378720aa0da6603cffed36f1
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-24 09:36:24 -0500

    solve problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    } else {
+        let newNum = parseInt(num);
+        let sequenceArray = [1, 1];
+        let sum;
+        if (newNum == 1 || newNum == 2) {
+            return 1;
+        } else {
+            for (let i = 0; i < newNum - 2; i++) {
+                sum = sequenceArray[sequenceArray.length - 1] + sequenceArray[sequenceArray.length - 2]
+                sequenceArray.push(sum);
+            }
+        }
+        return sum;
+    } 
 };
+
+fibonacci("1")
 
 // Do not edit below this line
 module.exports = fibonacci;
